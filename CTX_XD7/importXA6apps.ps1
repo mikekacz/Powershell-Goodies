2a70668ab5681ec4ae4a7c5f43444b77b146b733
mikekacz <mikekacz@gmail.com>
2017-05-17 18:03:48 +0200

    expanded XenApp 6.5 command to obain all details

--- CTX_XD7/importXA6apps.ps1
+++ CTX_XD7/importXA6apps.ps1
@@ -1,14 +1,13 @@
 Add-PSSnapin citrix.*
-get-brokersite -AdminAddress brokerHostnale
+get-brokersite -AdminAddress $brokerHostname
 
 <#
     prepare your pathToYourXMLfile by running following command on your XA6.x infrastructure:
-    Get-XAApplication | select applicationType, displayName, enabled, CommandLineExecutable, WorkingDirectory, ICON, Accounts, @{n="ServerNames";e={($_|get-xaserver).servername}},  @{n="WorkerGroupNames";e={($_|get-xaworkergroup).WorkerGroupName}}
+    Get-XAApplication | select applicationType, displayName, enabled, CommandLineExecutable, WorkingDirectory, @{n="icon";e={($_ | Get-XAApplicationIcon).EncodedIconData}}, @{n="Accounts";e={($_|Get-XAApplicationReport).Accounts}}, @{n="ServerNames";e={($_|get-xaserver).servername}},  @{n="WorkerGroupNames";e={($_|get-xaworkergroup).WorkerGroupName}} | export-clixml $pathToYourXMLfile
 #>
 
-
-
-$applist = import-clixml pathToYourXMLfile
+$applist = import-clixml $pathToYourXMLfile
+$mainDesktopGroup = "CitrixDesktop" #name of your Desktop Group to contain all published apps
 
 
 if ($false)
@@ -19,7 +18,7 @@ Foreach ($app in $($applist | ? {$_.applicationtype.value -eq 'ServerInstalled'}
     $name = $app.DisplayName.padright(32).Substring(0,32).trimend()
     
 
-    $newapp = New-BrokerApplication -BrowserName  "XD7 $name" -CommandLineExecutable  $app.CommandLineExecutable -DesktopGroup $(get-brokerdesktopgroup "Desktop ONE DG") -Name "XD7 $name" -WorkingDirectory $app.WorkingDirectory -Enabled $app.enabled -IconUid  $newicon.uid 
+    $newapp = New-BrokerApplication -BrowserName  "XD7 $name" -CommandLineExecutable  $app.CommandLineExecutable -DesktopGroup $(get-brokerdesktopgroup $mainDesktopGroup) -Name "XD7 $name" -WorkingDirectory $app.WorkingDirectory -Enabled $app.enabled -IconUid  $newicon.uid 
 
     foreach ($ADuser in @($app.Accounts.AccountDisplayName) )
     {
@@ -35,13 +34,16 @@ Foreach ($app in $($applist | ? {$_.applicationtype.value -eq 'ServerInstalled'}
 
 if ($true)
 {
+
+$mainDesktopGroupUID = $(get-brokerdesktopgroup $mainDesktopGroup).uid
+
 Foreach ($app in $($applist | ? {$_.applicationtype.value -eq 'ServerDesktop'}))
 {
     $name = $app.DisplayName.padright(32).Substring(0,32).trimend()
     $name = "XD7 $name"
-    $desktop = New-BrokerEntitlementPolicyRule   -Description "" -DesktopGroupUid 2 -Enabled $True -IncludedUserFilterEnabled $true -IncludedUsers  @($app.Accounts.AccountDisplayName) -Name "$name" -PublishedName "$name"
+    $desktop = New-BrokerEntitlementPolicyRule   -Description "" -DesktopGroupUid $mainDesktopGroupUID -Enabled $True -IncludedUserFilterEnabled $true -IncludedUsers  @($app.Accounts.AccountDisplayName) -Name "$name" -PublishedName "$name"
 
-    Read-Host "ant:"
+    #Read-Host "ant:"
 
 }
 }
