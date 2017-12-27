0a10131bfcffce259813525cf16ca3eee7e5aea7
mikekacz <mikekacz@gmail.com>
2017-12-27 15:59:39 +0100

    added Write-progress

--- JobQueue.ps1
+++ JobQueue.ps1
@@ -15,25 +15,25 @@ function Start-JobQueue {
     $i = 0 #Job index
 
     while ($i -lt $computername.Count -or @(get-job -HasMoreData $true).Count -ne 0 -or @(get-job | where-object state -eq 'Running').Count -ne 0 ) {
-        #TODO: what about job that are still running, but no data on them - Done
+        #do as long: not all computernames are added, there is still data in jobs, or there are Jobs running
         $jobs = @(get-job)
         if (@($jobs | Where-Object state -eq 'Running').count -lt $MaxThreads -and $i -lt $computername.Count) {
             #add next job
             $scriptBlockParameter[0] = $computername[$i]
             Start-Job -ScriptBlock $scriptBlock -Name $computername[$i] -ArgumentList $scriptBlockParameter | Out-Null
-            Write-Host "Adding job: $($computername[$i])" #TODO: move to Write-progress
             $i++
         }
 
         $jobs | Where-Object HasMoreData -eq $true | Receive-Job | Write-Output
         $jobs | Where-Object {$_.state -eq 'Running' -and ($(get-Date) - $_.PSBeginTime).totalseconds -gt $MaxRuntime } | Stop-Job 
 
+        Write-progress -activity "JobQueue" -completed $false -status "Total: $($computername.count); Running: $(@($jobs | Where-Object state -eq 'Running').count); Completed: $(@($jobs | Where-Object state -eq 'Completed').count); Stopped: $(@($jobs | Where-Object state -eq 'Stopped').count)"
         Start-Sleep -s $SleepTimer
     }
 
     #list failed jobs
     get-job | Where-Object state -ne "completed" | ForEach-Object {
-        Write-Warning -Message "Job: '$($_.name)' failed with status '$($_.state)'"
+        #Write-Warning -Message "Job: '$($_.name)' failed with status '$($_.state)'"
         $message = @{
             computername = $_.name
             status       = "failed with status '$($_.state)'"
