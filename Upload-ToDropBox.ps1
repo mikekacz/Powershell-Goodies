0c4fb27a29f1bfc195d2c606b7ebf6c62b1deeac
mikekacz <mikekacz@gmail.com>
2018-01-04 10:39:17 +0100

    added Upload-ToDropBox

---
+++ Upload-ToDropBox.ps1
@@ -0,0 +1,32 @@
+function upload-todropbox
+{
+<#
+.SYNOPSIS
+This is a Powershell script to upload a file to DropBox using their REST API.
+.DESCRIPTION
+This Powershell script will upload file to DropBox using their REST API with the parameters you provide.
+.PARAMETER SourceFilePath
+The path of the file to upload.
+.PARAMETER TargetFilePath
+The path of the file on DropBox.
+.ENV PARAMETER DropBoxAccessToken
+The DropBox access token.
+#>
+
+Param(
+    [Parameter(Mandatory=$true)]
+    [string]$SourceFilePath,
+    [Parameter(Mandatory=$true)]
+    [string]$TargetFilePath
+)
+
+$arg = '{ "path": "' + $TargetFilePath + '", "mode": "add", "autorename": true, "mute": false }'
+$authorization = "Bearer " + ($DropBoxAccessToken)
+$authorization
+$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$headers.Add("Authorization", $authorization)
+$headers.Add("Dropbox-API-Arg", $arg)
+$headers.Add("Content-Type", 'application/octet-stream')
+ 
+Invoke-RestMethod -Uri https://content.dropboxapi.com/2/files/upload -Method Post -InFile $SourceFilePath -Headers $headers
+}
